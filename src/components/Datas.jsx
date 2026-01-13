34fe08501b149c6fe7a91c34623728cf3a9ef9ca
magni5scent <younglamar24@gmail.com>
2026-01-13 19:44:43 +0100

    final-touch2

--- src/components/Datas.jsx
+++ src/components/Datas.jsx
@@ -1,12 +1,16 @@
 //This file contains an array which has data regarding the images like image source, name and individual address it gets imported by GameBoard.js.
 
-
 export default function Data({ score, bestScore }) {
   return (
     <div className="spread">
-      Best-Score:<span id="bestScore">{bestScore}</span>
-      || Score:
-      <span id="score">{score}</span>
+      <>
+        Best-Score:<span id="bestScore">{bestScore}</span>
+      </>
+      <>
+        {" "}
+        Score:
+        <span id="score">{score}</span>
+      </>
     </div>
   );
 }
